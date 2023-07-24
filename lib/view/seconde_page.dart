8fc91a0d52e4cecff1b2ddb0d2b3733d087693b4
skweel <dissingardjino@icloud.com>
2023-07-24 17:43:20 +0200

    mise à jour

--- lib/view/seconde_page.dart
+++ lib/view/seconde_page.dart
@@ -8,8 +8,44 @@ class MySecondePage extends StatefulWidget {
 }
 
 class _MySecondePageState extends State<MySecondePage> {
+  //variables
+  int index = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: bodyPage(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (value){
+          setState(() {
+            index= value;
+          });
+
+        },
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.music_note),
+            label: "Musique"
+          ),
+
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: "Favoris"
+          ),
+
+        ],
+      ),
+    );
+  }
+
+  Widget bodyPage(){
+    switch(index){
+      case 0 : return Text("Afficher les musiques");
+      case 1: return Text("Afficher mes favoris");
+      default : return Text("coucou");
+    }
+
   }
 }
